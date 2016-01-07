91fc671c75921d0fec63d61269bc9b06dbef693c
曾少臣 <zxc262403@gmail.com>
2016-01-07 18:12:58 +0800

    1.use the default config

--- reinstall_tair.sh
+++ reinstall_tair.sh
@@ -2,7 +2,7 @@
 /opt/tair_rdb_bin/tair.sh stop_cs
 /opt/tair_rdb_bin/tair.sh stop_ds
 sleep 2 
-rm /home/caster/tair_rdb_bin/etc -rf
+#rm /home/caster/tair_rdb_bin/etc -rf
 cp -rf /home/caster/tair_rdb_bin/* /opt/tair_rdb_bin
 rm -f /opt/tair_rdb_bin/logs/*
 rm -f /opt/tair_rdb_bin/data/data/*
