4022af663e20243514aa6ad541b67c6c9a078453
Caster Zeng <caster_zeng@wushuu.com>
2016-11-14 16:33:50 +0800

    quick fix

--- reinstall_tair.sh
+++ reinstall_tair.sh
@@ -6,6 +6,7 @@ sudo cp -rf /home/caster/tair_rdb_bin /opt/
 sudo chown -R  caster:caster /opt/tair_rdb_bin
 rm -f /opt/tair_rdb_bin/logs/*
 rm -f /opt/tair_rdb_bin/data/data/*
+chmod a+x /opt/tair_rdb_bin/tair.sh
 /opt/tair_rdb_bin/tair.sh start_ds
 /opt/tair_rdb_bin/tair.sh start_cs
 sleep 10 
